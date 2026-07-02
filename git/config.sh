298c8bfaff5a0b4257458a34aa110cbd48396ea1
hks816 <143579622+hks0704@users.noreply.github.com>
2026-07-02 23:26:19 +0900

    Create config.sh

---
+++ git/config.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# 이후 CI/CD 환경에서 git config 관련 설정을 추후에 추가
+
+# Jenkins나 배포 스크립트에서 경고가 안 나오도록
+# git config --system init.defaultBranch main
+
+# line ending 관련
+# git config --system core.autocrlf input
